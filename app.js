c5545fa372bb8d355106fcfae9af3856e942ac17
AndreaFZP <AndreaFZP@users.noreply.github.com>
2019-11-18 23:26:20 -0600

    Nuevas vistas añadidas: zapatos y +18

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var mongoose = require('mongoose');
 
 
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/home');
 var allRouter = require('./routes/all');
 var womenRouter = require('./routes/women');
 var menRouter = require('./routes/men');
+var shoesRouter = require('./routes/shoes');
+var ageRestrictedRouter = require('./routes/age18');
 var usersRouter = require('./routes/users');
 var usuarioRouter = require('./routes/usuario');
 var productoRouter = require('./routes/producto');
@@ -42,10 +44,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
+app.use('/home', indexRouter);
 app.use('/all', allRouter);
 app.use('/women', womenRouter);
 app.use('/men', menRouter);
+app.use('/shoes', shoesRouter);
+app.use('/age18', ageRestrictedRouter)
 app.use('/users', usersRouter);
 app.use('/usuario',usuarioRouter);
 app.use('/producto',productoRouter);
--- routes/shoes.js
+++ routes/shoes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req,res) =>{
-  res.render('index', { title: 'Home' });
+  res.render('shoes', { title: 'Shoes' });
 });
 
 module.exports = router;
