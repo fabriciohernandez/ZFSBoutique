e7b123a226714fa320a5f5d1835c823bacc9edf8
Fabrcio Hernandez <00163217@uca.edu.sv>
2019-11-26 23:36:52 -0600

    Auth all views

--- routes/all.js
+++ routes/all.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { ensureAuthenticated} = require('../config/auth')
 
-router.get("/", (req, res) => {
+router.get("/", ensureAuthenticated, (req, res) => {
   res.render("all", { title: "ALL" });
 });
 
